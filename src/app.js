b612e391c7b806cccf0da54f0d21bd2eb831f5f2
Abdur Rehman <abdur.rehman@mavensolutions.net>
2022-07-02 10:03:26 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const company = require('./sync/orcl_to_mdb');
+const sync_orcl_to_mdb = require('./sync/orcl_to_mdb');
 
-company.sync_setups();
+sync_orcl_to_mdb.sync_setups();
+sync_orcl_to_mdb.sync_trans();
 
   
--- src/sync/orcl_to_mdb.js
+++ src/sync/orcl_to_mdb.js
@@ -2,5 +2,21 @@ const db_utils = require('../db/utils');
 
 
 exports.sync_setups = () => {
-    db_utils.sync_orcl_to_mdb('COMPANY', ['COMP_CODE'])
+    db_utils.sync_orcl_to_mdb('COMPANY', ['COMP_CODE']);
+    db_utils.sync_orcl_to_mdb('PRODUCT', ['CODE', 'COMP_CODE']);
+    db_utils.sync_orcl_to_mdb('DISTRIBUTOR', ['CODE', 'COMP_CODE']);
+    // db_utils.sync_orcl_to_mdb('CLAIMTYPE', ['CODE']);
+}
+
+exports.sync_trans = () => {
+    db_utils.sync_orcl_to_mdb('PORDER_MASTER', [`COMP_CODE`, `DIST`, `MON`, `YEAR`, `SEQ_ID`]);
+    db_utils.sync_orcl_to_mdb('PORDER_DETAIL', [`COMP_CODE`, `DIST`, `MON`, `YEAR`, `SEQ_ID`, `PRODUCT`]);
+    db_utils.sync_orcl_to_mdb('SORDER_MASTER', [`TRN_NO`]);
+    db_utils.sync_orcl_to_mdb('SORDER_DETAIL', [`ID`]);
+
+    db_utils.sync_orcl_to_mdb('SPECIAL_ORDER_MASTER', [`COMP_CODE`, `DIST`, `MON`, `YEAR`, `SEQ_ID`]);
+    db_utils.sync_orcl_to_mdb('SPECIAL_ORDER_DETAIL', [`COMP_CODE`, `DIST`, `MON`, `YEAR`, `SEQ_ID`, `PRODUCT`]);
+
+    db_utils.sync_orcl_to_mdb('INS_PORDER_MASTER', [`COMP_CODE`, `DIST`, `YEAR`, `MON`, `SEQ_ID`]);
+    db_utils.sync_orcl_to_mdb('INS_PORDER_DETAIL', [`COMP_CODE`, `DIST`, `YEAR`, `MON`, `SEQ_ID`, `PRODUCT`]);
 }
