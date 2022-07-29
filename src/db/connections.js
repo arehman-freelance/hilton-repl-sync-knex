80430ded7b6e947983979dded5cfb4c4aca24eb7
Abdur Rehman <abdur.rehman@mavensolutions.net>
2022-07-29 12:11:28 +0000

    update for oracle db

--- src/db/connections.js
+++ src/db/connections.js
@@ -1,25 +1,28 @@
 const knex = require('knex');
 
 exports.knex_mariadb = knex({
-    client: 'mysql',
-    connection: {
-      host: 'mariadb',
-      user: 'root',
-      password: '123',
-      database: 'hilton_farrukh'
-    },
-    pool: { min: 0, max: 7 }
-  });
+  client: 'mysql',
+  connection: {
+    host: 'mariadb',
+    user: 'root',
+    password: '123',
+    database: 'hilton_farrukh'
+  },
+  pool: { min: 0, max: 7 }
+});
 
-  exports.knex_oracle = knex(  {
-    client: 'oracledb',
-    connection: {
-        user : 'HILTON',
-        password : 'HILTON',
-        connectString: 'oracledb/XE'
-      },
-    fetchAsString: [ 'number', 'clob', 'date' ],
-    pool: { min: 0, max: 7 }
-  });  
+exports.knex_oracle = knex({
+  client: 'oracledb',
+  connection: {
+    user: 'HILTON',
+    password: 'HILTON',
+    connectString: 'oracledb/XE'
+  },
+  fetchAsString: ['number', 'clob', 'date'],
+  pool: { min: 0, max: 7 },
+  // convert all identifiers to uppercase (Oracle standard)
+  wrapIdentifier: (value, origImpl, queryContext) => origImpl(value.toUpperCase())
 
-  
+});
+
+
